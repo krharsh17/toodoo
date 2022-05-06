2b4083d3b5708d7a83b04b3a6f3ad08af16a1d5c
Kumar Harsh <kharsh39@gmail.com>
2022-05-06 16:20:07 +0530

    TD #1: Integrate CapacitorJS and create GitHub action to automate android app bundle generation on all releases

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'me.kumarharsh.toodoo',
+  appName: 'toodoo',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
